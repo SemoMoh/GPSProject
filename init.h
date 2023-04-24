37a75925fbd136c43c92f1e2586e8b4133ee7de8
karimelnekheily <110358906+karimelnekheily@users.noreply.github.com>
2023-04-24 10:42:38 +0200

    Update init.h

--- init.h
+++ init.h
@@ -10,6 +10,7 @@
 void UART0_init(void);           // Function to initialise UART0 and GPIO for PA0 and PA1
 void UART5_init(void);           // Function to initialise UART5 and GPIO for PE4 and PE5
 void setCLkFreq(uint8_t f);      // Function to set a needed clock frequency
+void GPIO_PF_init(void);         // Enables the clock for Port F and waits for it to be ready. Unlocks Port F and disables the analog function for pins PF0 to PF4
 
 
 #endif /* INIT_H_ */
