9e740394b89f90fde8e377feb0e2ef140b04cfce
SemoMoh <128331219+SemoMoh@users.noreply.github.com>
2023-04-24 10:33:30 +0200

    Update and rename UART_init.h to init.h

--- init.h
+++ init.h
@@ -5,12 +5,12 @@
  *      Author: eslam
  */
 
-#ifndef UART_INIT_H_
-#define UART_INIT_H_
+#ifndef INIT_H_
+#define INIT_H_
 
 void UART0_init(void);           // Function to initialise UART0 and GPIO for PA0 and PA1
 void UART5_init(void);           // Function to initialise UART5 and GPIO for PE4 and PE5
 void setCLkFreq(uint8_t f);      // Function to set a needed clock frequency
 
 
-#endif /* UART_INIT_H_ */
+#endif /* INIT_H_ */
