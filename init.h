d467042200bbaa733b6ac9dcc4c2cea26198eaf1
SemoMoh <128331219+SemoMoh@users.noreply.github.com>
2023-04-24 10:35:05 +0200

    Update init.h

--- init.h
+++ init.h
@@ -1,6 +1,12 @@
 #ifndef INIT_H_
 #define INIT_H_
 
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "driverlib/sysctl.h"
+
 void UART0_init(void);           // Function to initialise UART0 and GPIO for PA0 and PA1
 void UART5_init(void);           // Function to initialise UART5 and GPIO for PE4 and PE5
 void setCLkFreq(uint8_t f);      // Function to set a needed clock frequency
