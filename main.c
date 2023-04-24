bf4695b7aa815560c2e4ec5ba13a8ca609fc4b3a
MohamedAhamed2100 <107947222+MohamedAhamed2100@users.noreply.github.com>
2023-04-24 16:40:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,20 +2,49 @@
 /**
  * Global Variables
  */
+double latEnd;
+double lonEnd;
 char strGPS[80];
-
+double longitude;
+double latitude;
+char latSTR[10];
+char lonSTR[10];
+double altitude;
+char time[10];
+double speed;
+int fix;
+char*token;
+int fieldCount;
+char receivedChar;
+int i;
+double latPre;
+double lonPre;
+double totalDist;
+bool reached;
+uint32_t strGPS_counter;
 
 /**
  * main function
  */
-int main(){
 
-    setCLkFreq(16); // set freq to 16Mhz
-    UART0_init();
-    UART5_init();
+int main(){
+    // Set the clocking to run directly from the internal oscillator at 16 MHz
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_INT | SYSCTL_MAIN_OSC_DIS);
+
+    Putty_init(); // Initialize UART0
+    GPS_init(); // Initialize UART5
+    GPIO_PFinit(); //Initiallize switch and LEDs
 
     while(1){
+        if(reached == true){ // when reached
 
-    }
+        }
+        
+        else{
+       readGPSString();
+       NMEA_Type();
+       decisionLED();
+       calcDistAcc();
+       }
+   }
 }
-
