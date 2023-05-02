d66da634f135598baac9277cf5d67e93418b5c35
Abdelrahman Elsayed <108687433+D3cipherd@users.noreply.github.com>
2023-05-02 23:39:06 +0300

    Add files via upload

---
+++ lcd.h
@@ -0,0 +1,32 @@
+#ifndef LCD_H_
+#define LCD_H_
+
+#define LCDPORT         GPIO_PORTB_BASE
+#define LCDPORTENABLE   SYSCTL_PERIPH_GPIOB
+#define RS              GPIO_PIN_0
+#define E               GPIO_PIN_1
+#define D4              GPIO_PIN_4
+#define D5              GPIO_PIN_5
+#define D6              GPIO_PIN_6
+#define D7              GPIO_PIN_7
+
+#define ROW1    0x80
+#define ROW2    0xC0
+#define ROW3    0x94
+#define ROW4    0xD4
+
+
+void LCD_init(void);                                //LCD initialization
+void LCD_Command(unsigned char c);                  //Send command
+void LCD_Show(unsigned char d);                     //Show a char
+void LCD_Clear(void);                               //Clear the screen
+void LCD_ClearChar(char row, char col);             //Clear specific character from position (row, col)
+void LCD_PrintLn(char row, char *s);                //Print specific line from beginning of row
+void LCD_PrintByPos(char row, char col, char *s);   //Print specific line from position (row, col)
+void LCD_PrintJustify(char i, char *s, char *d);    //Print specific line with 2 texts indented by x
+void LCD_Cursor(char row, char col);                //Set cursor
+void DelaySec(double s);                          //Delay by (s) seconds / e.g (s=1) -> 1 second
+void LCD_Menu();                                    //Print gate 2, gate 3 , gate 4
+void LCD_ReadDestination();                         //Read switch input and choose specific gate
+
+#endif /* LCD_H_ */
