06d5375d705ba3993334d5eece1ffa0a857c09df
UmarTahanix <119078558+UmarTahanix@users.noreply.github.com>
2023-05-06 13:23:19 +0300

    Update GPIO.h

--- GPIO.h
+++ GPIO.h
@@ -9,7 +9,7 @@
 
 bool sw1_input(void);                  //This function reads the input from switch 1 (PF4) and returns 1 if the switch is not pushed and 0 if it is pushed.
 bool sw2_input(void);                 //This function reads the input from switch 2 (PF0) and returns 1 if the switch is not pushed and 0 if it is pushed.
-void RGP_output(unsigned char data); //This function outputs the given data to the RGB LEDs. It resets the RGB LEDs and then turns on the needed LEDs based on the given data.
+void RGB_output(unsigned char data); //This function outputs the given data to the RGB LEDs. It resets the RGB LEDs and then turns on the needed LEDs based on the given data.
 
 
 
