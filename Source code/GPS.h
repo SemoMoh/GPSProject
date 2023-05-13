e051a74ad61853156204e9e67c97907960dbfd2e
jokr0000 <131316529+jokr0000@users.noreply.github.com>
2023-05-13 17:48:51 +0300

    Update GPS.h

--- Source code/GPS.h
+++ Source code/GPS.h
@@ -21,6 +21,11 @@ void GPS_init(void); // Initializes the GPS module
 uint8_t readGPSChar(); // Reads a single character from the GPS module 
 void readGPSString(); // Reads a string of characters from the GPS module 
 
+double calcDistBetween ( double latComp , double lonComp ); // Calculates the distance between two coordinates in meters 
+double calcDistBetweenPoints( double latComp1 , double lonComp1,double latComp , double lonComp); 
+double calcDistToEnd(); // Calculates the distance to the end point in meters 
+void calcDistAcc(); // Calculates the distance moved in meters 
+void decisionLED(); // Determines which LED to light up based on the distance to end 
 void showDirct(); // Displays the direction of travel on the LCD 
 double absolute(double lat, double lg); //calculate the absolute of a vector 
 
