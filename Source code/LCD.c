1eb858cd548beae1d6108361758bbece6b6f8f9e
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-05-13 16:49:24 +0300

    Update LCD.c
    Added setTargetDesitination 
    Added LCD_TargetReachedMenu
    Added LCD_ReadDistance

--- Source code/LCD.c
+++ Source code/LCD.c
@@ -2,6 +2,12 @@
 
 // Global Variables
 extern int fix; // Fixation value (defined in another file) 
+extern double latEnd; // Target destination latitude value (defined in another file)
+extern double lonEnd; // Target destination longitude value (defined in another file)
+extern double longitude; // Longitude value (defined in another file)
+extern double latitude; // Latitude value (defined in another file)
+extern double totalDist; // Total distance travelled (defined in another file)
+extern char *direction ; // Direction to target destination string
 // Functions
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -285,4 +291,111 @@ void LCD_UpdateDistance() {
     //Update Direction
     showDirct();
     LCD_PrintLn(2,direction);
+}
+
+
+//------------------------------------------------> KArim i
+/**
+ * @brief Sets the target destination and displays it on the LCD display.
+ *
+ * @param x The index of the target destination.
+ *
+ * This function sets the target destination based on the value of the x argument,
+ * (value related to each gate), displays the latitude and longitude of the target 
+ * destination on the LCD display, and attempts to establish a GPS connection. 
+ * The function displays the status of the GPS connection on the LCD display.
+ */
+void LCD_setTargetDestination(int x) {
+    if (x == 1) {
+        /*Gate 2 */
+        latEnd = 30.0646014; // Set the destination latitude
+        lonEnd = 31.2773738; // Set the destination longitude
+        LCD_PrintLnByPos(1, 0, "lat: 30.0646014"); // Print the destination latitude
+        LCD_PrintLnByPos(2, 0, "long: 31.2773738"); // Print the destination longitude
+        LCD_PrintLnByPos(3, 0, "Status: Starting..."); // Print the status
+        //wait for signal
+        while (fix == 0) {
+            //read till fix = 1 (signal is found)
+            readGPSString();
+            NMEA_Type();
+        }
+        LCD_PrintLnByPos(3, 0, "Status: Connected."); // Update the status
+        DelaySec(2);
+    } else if (x == 2) {
+        /*Gate 3 */
+        latEnd = 30.063907; // Set the destination latitude
+        lonEnd = 31.2776239; // Set the destination longitude
+        LCD_PrintLnByPos(1, 0, "lat: 30.063907"); // Print the destination latitude
+        LCD_PrintLnByPos(2, 0, "long: 31.2776239"); // Print the destination longitude
+        LCD_PrintLnByPos(3, 0, "Status: Starting..."); // Print the status
+        //wait for signal
+        while (fix == 0) {
+            //read till fix = 1 (signal is found)
+            readGPSString();
+            NMEA_Type();
+        }
+        LCD_PrintLnByPos(3, 0, "Status: Connected."); // Update the status
+        DelaySec(2);
+    } else {
+        /*Gate 6*/
+        latEnd = 30.064828; // Set the destination latitude
+        lonEnd = 31.280305; // Set the destination longitude
+        LCD_PrintLnByPos(1, 0, "lat: 30.064828"); // Print the destination latitude
+        LCD_PrintLnByPos(2, 0, "long: 31.280305"); // Print the destination longitude
+        LCD_PrintLnByPos(3, 0, "Status: Starting..."); // Print the status
+        //wait for signal
+        while (fix == 0) {
+            //read till fix = 1 (signal is found)
+            readGPSString();
+            NMEA_Type();
+        }
+        LCD_PrintLnByPos(3, 0, "Status: Connected."); // Update the status
+        DelaySec(2);
+    }
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//------------------------------------------------> Karim i
+/**
+ * @brief Reads and displays the distance information on the LCD display.
+ *
+ * This function reads and displays the distance information on the LCD display
+ * by calling various functions to calculate the distance to the end point and
+ * the total distance moved.
+ */
+void LCD_ReadDistance() {
+    LCD_Clear();
+    // print_Current_Position();
+
+    LCD_PrintLn(0, "DistToEnd:");
+    doubleToString(calcDistToEnd());
+    LCD_PrintLnByPos(0, 10, globalArray); // Print the distance to the end point
+
+
+    calcDistAcc(); // Calculate the distance moved
+    LCD_PrintLn(1, "DistMoved:");
+    doubleToString(totalDist);
+    LCD_PrintLnByPos(1, 10, globalArray); // Print the total distance moved
+
+    LCD_PrintLn(2,direction);
+}
+
+
+//------------------------------------------------> Karim i
+/**
+ * @brief Displays the target reached menu on the LCD display.
+ *
+ * This function updates the distance information on the LCD display by calling
+ * various functions to calculate the distance to the end point and the total
+ * distance moved.
+ */
+void LCD_TargetReachedMenu() {
+    LCD_Clear();
+    LCD_PrintLn(0, "Target Reached ;)");
+    LCD_PrintLn(2, "DistMoved: "); // Print "DistanceMoved:"
+    calcDistAcc(); // Calculate the distance moved
+    doubleToString(totalDist);
+    LCD_PrintLnByPos(2, 10, globalArray); // Print the total distance moved
 }
