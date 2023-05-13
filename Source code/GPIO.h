b234901af408b710014dd88d7a393f24d6c61d14
abdelrhman-ahmedsayed <99475686+abdelrhman-ahmedsayed@users.noreply.github.com>
2023-05-13 14:51:50 +0300

    Update GPIO.h

--- Source code/GPIO.h
+++ Source code/GPIO.h
@@ -1,1 +1,10 @@
-
+// This header file defines functions for initializing GPIO pins, reading switch inputs, and controlling RGB output.
+#ifndef GPIO_H_
+#define GPIO_H_
+// Include necessary header files
+#include "inc/tm4c123gh6pm.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "driverlib/sysctl.h"
