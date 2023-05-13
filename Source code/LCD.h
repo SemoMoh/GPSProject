d597f3cfdf23e7ae088a43c638401287228aa5e7
abdelrhman-ahmedsayed <99475686+abdelrhman-ahmedsayed@users.noreply.github.com>
2023-05-13 15:52:13 +0300

    Update LCD.h

--- Source code/LCD.h
+++ Source code/LCD.h
@@ -1,1 +1,17 @@
+#ifndef LCD_H_
+#define LCD_H_
 
+// Include necessary header files
+#include "inc/tm4c123gh6pm.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "driverlib/sysctl.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include <string.h>
+#include "GPS.h"
+// Function prototypes
+void LCD_Menu(); //Print gate 2, gate 3 , gate 4
+void LCD_ReadDestination(); //Read switch input and choose specific gate 
+void LCD_UpdateDistance(); // Update the distance on the LCD ----------> Abdo twin
